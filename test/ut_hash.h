c7f52b8937bf84c271965f2d4c52c433d15f6361
Rain <hxrain@users.noreply.github.com>
2018-08-11 16:25:04 +0800

    调整了hash整数测试用例,与随机数发生器进行了简单的对比,普遍比rand强.

--- test/ut_hash.h
+++ test/ut_hash.h
@@ -4,24 +4,46 @@
 #include "../rx_tdd.h"
 #include "../rx_hash_int.h"
 
-inline void rx_hash_int_base_1(rx_tdd_base &rt)
+
+inline void rx_hash_int_base_1(uint32_t seed,const char* hash_name,rx_hash32_func_t hash,rx_tdd_base &rt)
 {
-    uint32_t h = 1;
-    for (uint32_t i = 0; i < 10; ++i)
+    const uint32_t array_size = 100;
+    const uint32_t loop_count = 50000000;
+
+    uint32_t count_array[array_size];
+    memset(count_array,0,sizeof(count_array));
+
+    uint32_t h = seed;
+    for (uint32_t i = 0; i < loop_count; ++i)
     {
-        h = rx_hash_skeeto_f(i+h);
-        printf("hash(%4u)=0x%08x; mod 10=%4u \n", i, h,h%10);
+        h = hash(h+i);
+        uint32_t idx = h%array_size;
+        ++count_array[idx];
     }
-    for (uint32_t i = 0; i < 10; ++i)
+
+    uint32_t want_count = loop_count/ array_size;
+    uint64_t variance = 0;
+    for (uint32_t i = 0; i < array_size; ++i)
     {
-        srand(i);
-        printf("rand %4u\n",rand()%10);
+        uint32_t c= count_array[i];
+        //printf("hash func <%20s> num<%3u> count = %8u\n",hash_name,i,c);
+        variance += (want_count - c)*(want_count - c);
     }
+    printf("hash func <%20s> var = %.4f\n", hash_name, (double)variance / loop_count);
 }
 
-rx_tdd(rx_hash_int_base)
+inline uint32_t tmp_test_stdrand(uint32_t x)
 {
-    rx_hash_int_base_1(*this);
+    return rand();
+}
+
+rx_tdd_rtl(rx_hash_int_base,rtl_3)
+{
+    uint32_t seed = (uint32_t)time(NULL);
+    for(int i=0;i<IHT_Count;++i)
+        rx_hash_int_base_1(seed,rx_int_hash_name((rx_int_hash_type)i),rx_int_hash((rx_int_hash_type)i),*this);
+    srand(seed);
+    rx_hash_int_base_1(seed,"stdrand", tmp_test_stdrand,*this);
 }
 
 
