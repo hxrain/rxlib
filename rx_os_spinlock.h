c52ff075766e730ca323d2cebe19cc81e883fa23
Rain <hxrain@users.noreply.github.com>
2018-08-16 16:27:00 +0800

    小的修改.

--- rx_os_spinlock.h
+++ rx_os_spinlock.h
@@ -36,23 +36,20 @@ namespace rx
                     {//先进行CPU级的短休眠尝试
                         for (int32_t pause_cnt = spin_count; pause_cnt > 0; --pause_cnt)
                             rx_mm_pause();                  // 这是为支持超线程的 CPU 准备的切换提示
-                        spin_count *= 2;
+                        spin_count = spin_count<<1;         // 短自旋次数指数翻倍
                     }
                     else 
-                    {
+                    {//进行一定周期性的调度休眠与切换
                         uint32_t yield_cnt = loop_count - YIELD_THRESHOLD;
-                        if ((yield_cnt & 63) == 63) {
+                        if ((yield_cnt & 63) == 63) 
                             rx_thread_yield_us(1);          // 真正的休眠, 转入内核态
-                        }
-                        else if ((yield_cnt & 3) == 3) {
+                        else if ((yield_cnt & 3) == 3) 
                             rx_thread_yield_us(0);          // 切换到优先级跟自己一样或更高的线程, 可以换到别的CPU核心上
-                        }
-                        else {
+                        else 
                             if (!rx_thread_yield())         // 让步给该线程所在的CPU核心上的别的线程,
                                 rx_thread_yield_us(0);      // 如果同核心上没有可切换的线程,
-                        }
                     }
-                    loop_count++;
+                    ++loop_count;
                 } while (!rx_atomic_cas(&m_lock, 0, 1));
             }
             return true;
