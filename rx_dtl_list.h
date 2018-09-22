099ee07ee0ad737ebe3a047cb0833ad64cb3f464
Rain <hxrain@users.noreply.github.com>
2018-09-23 01:46:20 +0800

    以raw_queue_t为基础进行了队列容器的封装与统一测试用例的编写. 待进行完整功能测试.

--- rx_dtl_list.h
+++ rx_dtl_list.h
@@ -10,17 +10,18 @@
 namespace rx
 {
     //-----------------------------------------------------
-    //简单的栈容器(或单向链表)定义
+    //简单的栈容器(或单向链表,头部访问)定义
+    //-----------------------------------------------------
     template<class DT>
     class stack_t
     {
         //-------------------------------------------------
         template<class dt,typename dummy_t>
-        struct raw_stack_node_t
+        struct raw_node_t
         {
             typedef DT node_data_t;
+            struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
-            struct raw_stack_node_t* volatile next;	            //节点的后趋
             //---------------------------------------------
             //计算data需要的扩展尺寸
             template<class KT> static int ext_size(const KT &data){return 0;}
@@ -31,11 +32,11 @@ namespace rx
 
         //-------------------------------------------------
         template<typename dummy_t>
-        struct raw_stack_node_t<const char*,dummy_t>
+        struct raw_node_t<const char*,dummy_t>
         {
             typedef tiny_string_head_ct node_data_t;
+            struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
-            struct raw_stack_node_t* volatile next;	            //节点的后趋
             //---------------------------------------------
             //计算data需要的扩展尺寸
             template<class KT> static int ext_size(const KT &data){return st::strlen(data)+1;}
@@ -45,17 +46,17 @@ namespace rx
             {
                 char *buf=(char*)ext_buff;
                 ct::OC(&data,ext_size,buf);
-                data.set(val);
+                data.set(val,ext_size-1);
             }
         };
 
         //-------------------------------------------------
         template<typename dummy_t>
-        struct raw_stack_node_t<const wchar_t*,dummy_t>
+        struct raw_node_t<const wchar_t*,dummy_t>
         {
             typedef tiny_string_head_wt node_data_t;
+            struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
-            struct raw_stack_node_t* volatile next;	            //节点的后趋
             //---------------------------------------------
             //计算data需要的扩展尺寸
             template<class KT> static int ext_size(const KT &data){return st::strlen(data)+1;}
@@ -65,18 +66,18 @@ namespace rx
             {
                 wchar_t *buf=(wchar_t*)ext_buff;
                 ct::OC(&data,ext_size,buf);
-                data.set(val);
+                data.set(val,ext_size-1);
             }
         };
-
-        //-------------------------------------------------
-        typedef raw_stack_node_t<DT,void>   raw_node_t;         //最终使用的原始栈节点类型
-        typedef raw_stack_t<raw_node_t>     raw_stack_t;        //最终使用的原始栈容器类型
-        //-------------------------------------------------
-        mem_allotter_i  &m_mem;                                  //内存分配器,引用
-        raw_stack_t     m_stack;                                //底层栈容器
     public:
-        typedef raw_node_t node_t;
+        //-------------------------------------------------
+        typedef raw_node_t<DT,void>     node_t;             //最终使用的原始栈节点类型
+    protected:
+        typedef raw_stack_t<node_t>     cntr_t;             //最终使用的原始栈容器类型
+        //-------------------------------------------------
+        mem_allotter_i  &m_mem;                             //内存分配器,引用
+        cntr_t          m_cntr;                             //底层栈容器
+    public:
         //-------------------------------------------------
         stack_t():m_mem(global_mem_allotter()){}
         stack_t(mem_allotter_i& ma):m_mem(ma){}
@@ -89,6 +90,7 @@ namespace rx
             friend class stack_t;
         public:
             //---------------------------------------------
+            iterator() :m_node(NULL) {}
             iterator(const node_t *node) :m_node(node) {}
             iterator(const iterator &i):m_node(i.m_node){}
             //---------------------------------------------
@@ -108,7 +110,7 @@ namespace rx
         };
         //-------------------------------------------------
         //查看栈顶;返回遍历的初始位置
-        iterator begin() const {return iterator(m_stack.head());}
+        iterator begin() const {return iterator(m_cntr.head());}
         //-------------------------------------------------
         //返回遍历的结束位置
         iterator end() const { return iterator(NULL); }
@@ -117,7 +119,7 @@ namespace rx
         void clear() {while(pop_front());}
         //-------------------------------------------------
         //栈内元素数量
-        uint32_t size() const {return m_stack.size();}
+        uint32_t size() const {return m_cntr.size();}
         //-------------------------------------------------
         //数据入栈,返回最新节点.
         template<class dt>
@@ -126,7 +128,7 @@ namespace rx
             uint32_t es=node_t::ext_size(data);
             node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
             if (!node) return iterator(NULL);
-            m_stack.push_front(node);
+            m_cntr.push_front(node);
             node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
             return iterator(node);
         }
@@ -136,7 +138,7 @@ namespace rx
         bool pop_front()
         {
             if (!size()) return false;
-            node_t *node=m_stack.pop_front();
+            node_t *node=m_cntr.pop_front();
             ct::OD(node);                               //节点析构
             m_mem.free(node);                           //节点内存释放
             return true;
@@ -151,6 +153,112 @@ namespace rx
     //语法糖,定义一个便于使用的const wchar_t*内容持有栈
     typedef stack_t<const wchar_t*>  stack_wstr_t;
 
+
+
+    //-----------------------------------------------------
+    //简单的队列容器(或单向链表,头尾快速访问)定义
+    //-----------------------------------------------------
+    template<class DT>
+    class queue_t
+    {
+    public:
+        typedef typename stack_t<DT>::node_t node_t;
+    protected:
+        //-------------------------------------------------
+        typedef raw_queue_t<node_t>         cntr_t;        //最终使用的原始栈容器类型
+        //-------------------------------------------------
+        mem_allotter_i  &m_mem;                            //内存分配器,引用
+        cntr_t           m_cntr;                           //底层栈容器
+    public:
+        //-------------------------------------------------
+        queue_t():m_mem(global_mem_allotter()){}
+        queue_t(mem_allotter_i& ma):m_mem(ma){}
+        virtual ~queue_t(){clear();}
+        //-------------------------------------------------
+        //定义简单的只读迭代器
+        class iterator
+        {
+            const node_t  *m_node;
+            friend class queue_t;
+        public:
+            //---------------------------------------------
+            iterator() :m_node(NULL) {}
+            iterator(const node_t *node) :m_node(node) {}
+            iterator(const iterator &i):m_node(i.m_node){}
+            //---------------------------------------------
+            bool operator==(const iterator &i)const { return m_node == i.m_node; }
+            bool operator!=(const iterator &i)const { return !(operator==(i));}
+            //---------------------------------------------
+            iterator& operator=(iterator &i) {m_node=i.m_node; return *this;}
+            //---------------------------------------------
+            //* 运算符重载,用于获取当前节点的data
+            const typename node_t::node_data_t& operator* () const {return m_node->data;}
+            //---------------------------------------------
+            iterator& operator++()
+            {
+                m_node=m_node->next;
+                return reinterpret_cast<iterator&>(*this);
+            }
+        };
+        //-------------------------------------------------
+        //查看栈顶;返回遍历的初始位置
+        iterator begin() const {return iterator(m_cntr.head());}
+        //-------------------------------------------------
+        //返回遍历的结束位置
+        iterator end() const { return iterator(m_cntr.origin()); }
+        //-------------------------------------------------
+        //查看队尾
+        iterator rbegin() const {return iterator(m_cntr.tail());}
+        //-------------------------------------------------
+        //释放全部节点
+        void clear() {while(pop_front());}
+        //-------------------------------------------------
+        //栈内元素数量
+        uint32_t size() const {return m_cntr.size();}
+        //-------------------------------------------------
+        //数据入队(队尾),返回最新节点.
+        template<class dt>
+        iterator push_back(const dt &data)
+        {
+            uint32_t es=node_t::ext_size(data);
+            node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
+            if (!node) return end();
+            m_cntr.push_back(node);
+            node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
+            return iterator(node);
+        }
+        //-------------------------------------------------
+        //数据入栈(队首),返回最新节点.
+        template<class dt>
+        iterator push_front(const dt &data)
+        {
+            uint32_t es=node_t::ext_size(data);
+            node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
+            if (!node) return end();
+            m_cntr.push_front(node);
+            node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
+            return iterator(node);
+        }
+        //-------------------------------------------------
+        //弹出队首元素并释放.
+        //返回值:空栈失败,否则成功.
+        bool pop_front()
+        {
+            if (!size()) return false;
+            node_t *node=m_cntr.pop_front();
+            ct::OD(node);                               //节点析构
+            m_mem.free(node);                           //节点内存释放
+            return true;
+        }
+    };
+
+    //语法糖,定义一个便于使用的整数栈
+    typedef queue_t<uint32_t>        queue_uint32_t;
+    typedef queue_t<int32_t>         queue_int32_t;
+    //语法糖,定义一个便于使用的const char*内容持有栈
+    typedef queue_t<const char*>     queue_cstr_t;
+    //语法糖,定义一个便于使用的const wchar_t*内容持有栈
+    typedef queue_t<const wchar_t*>  queue_wstr_t;
 
 }
 
--- test/ut_dtl_list.h
+++ test/ut_dtl_list.h
@@ -4,14 +4,16 @@
 #include "../rx_tdd.h"
 #include "../../rx_dtl_list.h"
 
+//---------------------------------------------------------
+//进行容器的遍历,同时对比给定的数组元素内容
 template<class ct,class vt>
-inline void test_dtl_list_loop_test_1(rx_tdd_t &rt,const ct &s,vt* arr,uint32_t count,bool asc=true)
+inline void test_dtl_list_loop_test_1(rx_tdd_t &rt,const ct &s,vt* arr,uint32_t count,bool asc=true,uint32_t asc_offset=0)
 {
     rt.tdd_assert(s.size()==count);
     ct::iterator I=s.begin();
     if (asc)
     {
-        for(uint32_t i=0;I!=s.end();++i,++I)
+        for(uint32_t i=asc_offset;I!=s.end();++i,++I)
             rt.tdd_assert(*I==arr[i]);
     }
     else
@@ -20,110 +22,151 @@ inline void test_dtl_list_loop_test_1(rx_tdd_t &rt,const ct &s,vt* arr,uint32_t 
             rt.tdd_assert(*I==arr[count-i-1]);
     }
 }
-
-template<class ct>
-inline void test_dtl_stack_base_1(rx_tdd_t &rt)
+//---------------------------------------------------------
+//使用模板特化进行容器push动作的区分
+template<class cntr_t,bool asc>
+struct tst_list_push{};
+template<class cntr_t>
+struct tst_list_push<cntr_t,true>
 {
-    int tst_arr[10];
+    template<class DT>
+    static typename cntr_t::iterator push(cntr_t &s,const DT& val){return s.push_back(val);}
+    enum {is_back=true};
+};
+template<class cntr_t>
+struct tst_list_push<cntr_t,false>
+{
+    template<class DT>
+    static typename cntr_t::iterator push(cntr_t &s,const DT& val){return s.push_front(val);}
+    enum {is_back=false};
+};
+//---------------------------------------------------------
+//使用模板特化进行容器pop动作的区分
+template<class cntr_t,bool front>
+struct tst_list_pop{};
+template<class cntr_t>
+struct tst_list_pop<cntr_t,true>
+{
+    static bool pop(cntr_t &s){return s.pop_front();}
+    enum {is_front=true};
+};
+template<class cntr_t>
+struct tst_list_pop<cntr_t,false>
+{
+    static bool pop(cntr_t &s){return s.pop_back();}
+    enum {is_front=false};
+};
+//---------------------------------------------------------
+//使用模板特化进行容器last动作的区分
+template<class cntr_t,bool front>
+struct tst_list_last{};
+template<class cntr_t>
+struct tst_list_last<cntr_t,true>
+{
+    static typename cntr_t::iterator last(cntr_t &s){return s.begin();}
+    enum {is_front=true};
+};
+template<class cntr_t>
+struct tst_list_last<cntr_t,false>
+{
+    static typename cntr_t::iterator last(cntr_t &s){return s.rbegin();}
+    enum {is_front=false};
+};
+//---------------------------------------------------------
+//使用模板特化进行容器测试数据类型的区分
+template<class dt>
+struct tst_list_data{};
+template<>
+struct tst_list_data<int>
+{
+    static int val(int v){return v;}
+    typedef int val_t;
+};
+template<>
+struct tst_list_data<const char*>
+{
+    static const char* val(uint32_t v)
+    {
+        switch(v)
+        {
+        case 0:return "0";
+        case 1:return "1";
+        case 2:return "2";
+        case 3:return "3";
+        case 4:return "4";
+        }
+        return "BAD";
+    }
+    typedef const char* val_t;
+};
+
+//---------------------------------------------------------
+//对容器进行基础操作的测试,整数类型
+template<class ct,class dv,bool pushback,bool popfront,bool isback>
+inline void test_dtl_list_base_1(rx_tdd_t &rt)
+{
+    typedef tst_list_push<ct,pushback> pt;
+    typedef tst_list_pop<ct,popfront> ot;
+    typedef tst_list_last<ct,isback> lt;
+
+    dv::val_t tst_arr[10];
     uint32_t count=0;
     ct s;
     rt.tdd_assert(s.size()==0);
     rt.tdd_assert(s.begin()==s.end());
-    
-    tst_arr[count]=1;
-    ct::iterator I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
 
-    tst_arr[count]=2;
-    I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
+    tst_arr[count]=dv::val(1);
+    ct::iterator I;
+    I=pt::push(s,tst_arr[count++]);
+    rt.tdd_assert(I==lt::last(s));
     rt.tdd_assert(s.size()==count);
     rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
+    test_dtl_list_loop_test_1(rt,s,tst_arr,count,pt::is_back);
 
-    tst_arr[count]=3;
-    I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
+    tst_arr[count]=dv::val(2);
+    I=pt::push(s,tst_arr[count++]);
+    rt.tdd_assert(I==lt::last(s));
     rt.tdd_assert(s.size()==count);
     rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
+    test_dtl_list_loop_test_1(rt,s,tst_arr,count,pt::is_back);
+
+    tst_arr[count]=dv::val(3);
+    I=pt::push(s,tst_arr[count++]);
+    rt.tdd_assert(I==lt::last(s));
+    rt.tdd_assert(s.size()==count);
+    rt.tdd_assert(s.begin()!=s.end());
+    test_dtl_list_loop_test_1(rt,s,tst_arr,count,pt::is_back);
+    /*
+    --count;
+    rt.tdd_assert(*lt::last(s)==tst_arr[count]);
+    rt.tdd_assert(ot::pop(s));
+    rt.tdd_assert(s.size()==count);
+    rt.tdd_assert(s.begin()!=s.end());
+    test_dtl_list_loop_test_1(rt,s,tst_arr,count,!ot::is_front);
 
     --count;
-    rt.tdd_assert(tst_arr[count]==*s.begin());
-    rt.tdd_assert(s.pop_front());
+    rt.tdd_assert(*lt::last(s)==tst_arr[count]);
+    rt.tdd_assert(ot::pop(s));
     rt.tdd_assert(s.size()==count);
     rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    --count;
-    rt.tdd_assert(tst_arr[count]==*s.begin());
-    rt.tdd_assert(s.pop_front());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
+    test_dtl_list_loop_test_1(rt,s,tst_arr,count,!ot::is_front);
+    */
     s.clear();
     rt.tdd_assert(s.size()==0);
     rt.tdd_assert(s.begin()==s.end());
 }
-
-template<class ct>
-inline void test_dtl_stack_str_1(rx_tdd_t &rt)
-{
-    char* tst_arr[10];
-    uint32_t count=0;
-    ct s;
-    rt.tdd_assert(s.size()==0);
-    rt.tdd_assert(s.begin()==s.end());
-
-    tst_arr[count]="1";
-    ct::iterator I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    tst_arr[count]="2";
-    I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    tst_arr[count]="3";
-    I=s.push_front(tst_arr[count++]);
-    rt.tdd_assert(I==s.begin());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    --count;
-    rt.tdd_assert(*s.begin()==tst_arr[count]);
-    rt.tdd_assert(s.pop_front());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    --count;
-    rt.tdd_assert(*s.begin()==tst_arr[count]);
-    rt.tdd_assert(s.pop_front());
-    rt.tdd_assert(s.size()==count);
-    rt.tdd_assert(s.begin()!=s.end());
-    test_dtl_list_loop_test_1(rt,s,tst_arr,count,false);
-
-    s.clear();
-    rt.tdd_assert(s.size()==0);
-    rt.tdd_assert(s.begin()==s.end());
-}
-
 
 rx_tdd(dtl_list_base)
 {
-    test_dtl_stack_base_1<rx::stack_int32_t>(*this);
-    test_dtl_stack_str_1<rx::stack_cstr_t>(*this);
+    test_dtl_list_base_1<rx::stack_int32_t,tst_list_data<int>  ,false,true,true >(*this);//LIFO
+    test_dtl_list_base_1<rx::stack_cstr_t, tst_list_data<const char*>,false,true,true >(*this);//LIFO
+
+    test_dtl_list_base_1<rx::queue_int32_t,tst_list_data<int>  ,false,true,true >(*this);//LIFO
+    test_dtl_list_base_1<rx::queue_cstr_t, tst_list_data<const char*>,false,true,true >(*this);//LIFO
+
+    test_dtl_list_base_1<rx::queue_int32_t,tst_list_data<int>  ,true,true,false >(*this);//FIFO
+    test_dtl_list_base_1<rx::queue_cstr_t,tst_list_data<const char*> ,true,true,false >(*this);//FIFO
+
 }
 
 #endif
