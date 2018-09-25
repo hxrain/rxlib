23a90c55fc0f601fb462f31ddd1df19816e50ea0
Rain <hxrain@users.noreply.github.com>
2018-09-25 19:07:44 +0800

    增加了特定接口函数类型的委托回调功能封装,初步测试验证完成.

--- test/ut_ct_obj.h
+++ test/ut_ct_obj.h
@@ -17,8 +17,51 @@ public:
     tmp_tst_obj_2(int v):tmp_tst_obj_1(v){}
 };
 
+namespace rx_ut
+{
+    class cb_test_class_t
+    {
+    public:
+        uint32_t member_method(void *obj, void *p1, void *p2, void *usrdat) { printf("TEST DELEGATE :: call cb_test_class_t::member_method()\n"); return 20; }
+    };
+    uint32_t std_cb_func_t(void *obj, void *p1, void *p2, void *usrdat) { printf("TEST DELEGATE :: call std_cb_func_t()\n"); return 10; }
+
+    void test_delegate_base_1(rx_tdd_t &rt)
+    {
+        rx::delegate_def_t dg;
+
+        //进行标准函数的委托绑定与调用测试
+        dg.bind(std_cb_func_t);
+        rt.tdd_assert(dg.is_valid());
+        rt.tdd_assert(dg(NULL,0,0,0)==10);
+
+        //进行成员函数的委托绑定与调用测试
+        cb_test_class_t cb;
+        dg.bind(cb,&cb_test_class_t::member_method);
+        rt.tdd_assert(dg.is_valid());
+        rt.tdd_assert(dg(NULL, 0, 0, 0) == 20);
+    }
+
+    void test_delegate_base_2(rx_tdd_t &rt)
+    {
+        //进行标准函数的委托绑定与调用测试
+        rx::delegate_def_t dg(std_cb_func_t);
+        rt.tdd_assert(dg.is_valid());
+        rt.tdd_assert(dg(NULL, 0, 0, 0) == 10);
+
+        //进行成员函数的委托绑定与调用测试
+        cb_test_class_t cb;
+        rx::delegate_def_t dgc(cb, &cb_test_class_t::member_method);
+        rt.tdd_assert(dgc.is_valid());
+        rt.tdd_assert(dgc(NULL, 0, 0, 0) == 20);
+    }
+}
+
 rx_tdd(tmp_obj)
 {
+    rx_ut::test_delegate_base_1(*this);
+    rx_ut::test_delegate_base_2(*this);
+
     uint8_t buff_obj[sizeof(tmp_tst_obj_2)*5];
     tmp_tst_obj_2 *o2=(tmp_tst_obj_2*)buff_obj;
     rx::ct::OC(o2,2);
