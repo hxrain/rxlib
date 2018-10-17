0847664cb23a416c6410e8323a5fd4f23741f8a4
Rain <hxrain@users.noreply.github.com>
2018-10-17 23:05:37 +0800

    对dtl的list进行扩展;进行stack和queue功能封装的文件拆分.

--- rx_dtl_stack.h
+++ rx_dtl_stack.h
@@ -1,5 +1,5 @@
-#ifndef _RX_DTL_LIST_H_
-#define _RX_DTL_LIST_H_
+#ifndef _RX_DTL_STACK_H_
+#define _RX_DTL_STACK_H_
 
 #include "rx_cc_macro.h"
 #include "rx_dtl_list_raw.h"
@@ -9,33 +9,33 @@
 
 /*
     //-----------------------------------------------------
-    //简单的list容器(双向链表,头尾均可快速访问)
+    //简单的栈容器(或单向链表,头部访问)定义
     template<class DT>
-    class list_t;
+    class stack_t;
 
-    //语法糖,定义一个便于使用的整数队列
-    typedef list_t<uint32_t>        list_uint32_t;
-    typedef list_t<int32_t>         list_int32_t;
-    //语法糖,定义一个便于使用的const char*内容持有队列
-    typedef list_t<const char*>     list_cstr_t;
-    //语法糖,定义一个便于使用的const wchar_t*内容持有队列
-    typedef list_t<const wchar_t*>  list_wstr_t;
+    //语法糖,定义一个便于使用的整数栈
+    typedef stack_t<uint32_t>        stack_uint32_t;
+    typedef stack_t<int32_t>         stack_int32_t;
+    //语法糖,定义一个便于使用的const char*内容持有栈
+    typedef stack_t<const char*>     stack_cstr_t;
+    //语法糖,定义一个便于使用的const wchar_t*内容持有栈
+    typedef stack_t<const wchar_t*>  stack_wstr_t;
+
 */
 
 namespace rx
 {
     //-----------------------------------------------------
-    //简单的list容器(双向链表,头尾均可快速访问)
+    //简单的栈容器(或单向链表,头部访问)定义
     //-----------------------------------------------------
     template<class DT>
-    class list_t
+    class stack_t
     {
         //-------------------------------------------------
         template<class dt,typename dummy_t>
         struct raw_node_t
         {
             typedef DT node_data_t;
-            struct raw_node_t* volatile prev;	            //节点的前趋
             struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
             //---------------------------------------------
@@ -51,7 +51,6 @@ namespace rx
         struct raw_node_t<const char*,dummy_t>
         {
             typedef tiny_string_head_ct node_data_t;
-            struct raw_node_t* volatile prev;	            //节点的前趋
             struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
             //---------------------------------------------
@@ -72,11 +71,10 @@ namespace rx
         struct raw_node_t<const wchar_t*,dummy_t>
         {
             typedef tiny_string_head_wt node_data_t;
-            struct raw_node_t* volatile prev;	            //节点的前趋
             struct raw_node_t* volatile next;	            //节点的后趋
             node_data_t  data;
             //---------------------------------------------
-            //计算data需要的扩展尺寸
+            //计算data需要的扩展尺寸(是串长度+1的2倍)
             template<class KT> static int ext_size(const KT &data){return (st::strlen(data)+1) * sc<wchar_t>::char_size();}
             //---------------------------------------------
             //进行定向构造并初始化
@@ -90,24 +88,23 @@ namespace rx
         };
     public:
         //-------------------------------------------------
-        typedef raw_node_t<DT,void>     node_t;             //最终使用的原始栈节点类型    
+        typedef raw_node_t<DT,void>     node_t;             //最终使用的原始栈节点类型
     protected:
-        //-------------------------------------------------
-        typedef raw_list_t<node_t>      cntr_t;             //最终使用的原始栈容器类型
+        typedef raw_stack_t<node_t>     cntr_t;             //最终使用的原始栈容器类型
         //-------------------------------------------------
         mem_allotter_i  &m_mem;                             //内存分配器,引用
-        cntr_t           m_cntr;                            //底层栈容器
+        cntr_t          m_cntr;                             //底层栈容器
     public:
         //-------------------------------------------------
-        list_t():m_mem(rx_global_mem_allotter()){}
-        list_t(mem_allotter_i& ma):m_mem(ma){}
-        virtual ~list_t(){clear();}
+        stack_t():m_mem(rx_global_mem_allotter()){}
+        stack_t(mem_allotter_i& ma):m_mem(ma){}
+        virtual ~stack_t(){clear();}
         //-------------------------------------------------
         //定义简单的只读迭代器
         class iterator
         {
             const node_t  *m_node;
-            friend class list_t;
+            friend class stack_t;
         public:
             //---------------------------------------------
             iterator() :m_node(NULL) {}
@@ -127,120 +124,51 @@ namespace rx
                 m_node=m_node->next;
                 return reinterpret_cast<iterator&>(*this);
             }
-            iterator& operator--()
-            {
-                m_node=m_node->prev;
-                return reinterpret_cast<iterator&>(*this);
-            }
         };
         //-------------------------------------------------
-        //查看队首;返回遍历的初始位置
+        //查看栈顶;返回遍历的初始位置
         iterator begin() const {return iterator(m_cntr.head());}
         //-------------------------------------------------
         //返回遍历的结束位置
-        iterator end() const { return iterator(m_cntr.origin()); }
-        //-------------------------------------------------
-        //查看队尾,或反向遍历的起点
-        iterator rbegin() const {return iterator(m_cntr.tail());}
-        //-------------------------------------------------
-        //反向遍历的结束位置
-        iterator rend() const { return iterator(m_cntr.origin()); }
+        iterator end() const { return iterator(NULL); }
         //-------------------------------------------------
         //释放全部节点
         void clear() {while(pop_front());}
         //-------------------------------------------------
-        //内部元素数量
+        //栈内元素数量
         uint32_t size() const {return m_cntr.size();}
         //-------------------------------------------------
-        //数据入队(队尾),返回最新节点.
-        template<class dt>
-        iterator push_back(const dt &data)
-        {
-            uint32_t es=node_t::ext_size(data);
-            node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
-            if (!node) return end();
-            m_cntr.push_back(node);
-            node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
-            return iterator(node);
-        }
-        //数据入队(在指定节点it的后面),返回最新节点.
-        template<class dt>
-        iterator push_back(const dt &data,iterator it)
-        {
-            uint32_t es=node_t::ext_size(data);
-            node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
-            if (!node) return end();
-            m_cntr.push_back(it.m_node,node);
-            node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
-            return iterator(node);
-        }
-        //-------------------------------------------------
-        //数据入栈(队首),返回最新节点.
+        //数据入栈,返回最新节点.
         template<class dt>
         iterator push_front(const dt &data)
         {
             uint32_t es=node_t::ext_size(data);
             node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
-            if (!node) return end();
+            if (!node) return iterator(NULL);
             m_cntr.push_front(node);
             node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
             return iterator(node);
         }
-        //数据入栈(在指定节点it的前面),返回最新节点.
-        template<class dt>
-        iterator push_front(const dt &data,iterator it)
-        {
-            uint32_t es=node_t::ext_size(data);
-            node_t *node=(node_t *)m_mem.alloc(es+sizeof(node_t));
-            if (!node) return end();
-            m_cntr.push_front(it.m_node,node);
-            node->OC(data,((uint8_t*)node+sizeof(node_t)),es);
-            return iterator(node);
-        }
         //-------------------------------------------------
-        //弹出队尾元素并释放.
-        //返回值:空栈失败,否则成功.
-        bool pop_back()
-        {
-            if (!size()) return false;
-            node_t *node=m_cntr.pop_back();
-            ct::OD(node);                               //节点析构
-            m_mem.free(node);                           //节点内存释放
-            return true;
-        }
-        //-------------------------------------------------
-        //弹出队首元素并释放.
+        //弹出栈顶元素并释放.
         //返回值:空栈失败,否则成功.
         bool pop_front()
         {
             if (!size()) return false;
             node_t *node=m_cntr.pop_front();
-            ct::OD(node);                                   //节点析构
-            m_mem.free(node);                               //节点内存释放
-            return true;
-        }
-        //-------------------------------------------------
-        //删除指定节点,it指向其后续节点
-        //返回值:是否删除成功
-        bool earse(iterator &it)
-        {
-            if (!size()) return false;
-            node_t *node=m_cntr.pick(it.m_node)
-            ct::OD(it.m_node);                              //节点析构
-            m_mem.free(it.m_node);                          //节点内存释放
-
-            it.m_node=node;
+            ct::OD(node);                               //节点析构
+            m_mem.free(node);                           //节点内存释放
             return true;
         }
     };
 
-    //语法糖,定义一个便于使用的整数队列
-    typedef list_t<uint32_t>        list_uint32_t;
-    typedef list_t<int32_t>         list_int32_t;
-    //语法糖,定义一个便于使用的const char*内容持有队列
-    typedef list_t<const char*>     list_cstr_t;
-    //语法糖,定义一个便于使用的const wchar_t*内容持有队列
-    typedef list_t<const wchar_t*>  list_wstr_t;
+    //语法糖,定义一个便于使用的整数栈
+    typedef stack_t<uint32_t>        stack_uint32_t;
+    typedef stack_t<int32_t>         stack_int32_t;
+    //语法糖,定义一个便于使用的const char*内容持有栈
+    typedef stack_t<const char*>     stack_cstr_t;
+    //语法糖,定义一个便于使用的const wchar_t*内容持有栈
+    typedef stack_t<const wchar_t*>  stack_wstr_t;
 }
 
 #endif
