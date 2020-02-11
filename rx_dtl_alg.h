2b21b2e92902095faea42bcfee5fb123b8b89dec
Rain <hxrain@users.noreply.github.com>
2020-02-11 22:47:21 +0800

    增加了unicode blocks信息的查询功能. 附带增加了两种二分查找算法.

---
+++ rx_dtl_alg.h
@@ -0,0 +1,58 @@
+#ifndef _RX_DTL_ALG_H_
+#define _RX_DTL_ALG_H_
+
+#include "rx_cc_macro.h"
+
+namespace rx
+{
+    //针对元素类型ET的数组的二分搜索算法,在长度为length的arr数组中查找x(要求arr为正序)
+    //返回值:arrsize,未找到;其他为x在数组中的索引
+    template<class ET>
+    inline int bisect(const ET* arr,const uint32_t length,const ET& x) {
+        uint32_t left=0;
+        uint32_t right=length-1;
+        while(left<right) {
+            uint32_t mid=(left+right)/2;
+            const ET& ci=arr[mid];
+            if(ci==x)
+                return mid;
+            else if(ci>x)
+                right=mid-1;
+            else 
+                left=mid+1;
+        }
+        if (left==right&&arr[left]==x)
+            return left;
+        return length;
+    }
+
+    //针对元素类型ET的数组的二分搜索算法,在长度为length的arr数组中查找最左侧接近x的值索引(要求arr为正序)
+    //返回值:arrsize,未找到;其他为x在数组中的索引    
+    template<class ET>
+    inline uint32_t bisect_ll(const ET* arr,const uint32_t length,const ET& x)
+    {
+        uint32_t left = 0, right = length-1;
+        while(left < right)
+        {
+            uint32_t mid = (left+right)/2;
+            if(arr[mid] <= x)
+            {
+                if (left == mid)
+                    break;
+                left = mid;
+            }
+            else
+                right = mid;
+        }
+
+        if(arr[right] < x)
+            return right;
+        else if(arr[left] <= x)
+            return left;
+        
+        return length;
+    }
+}
+
+
+#endif
---
+++ test/ut_ucs_misc.h
@@ -0,0 +1,48 @@
+#ifndef _UT_UCS2_MISC_H_
+#define _UT_UCS2_MISC_H_
+
+#include "../rx_cc_macro.h"
+#include "../rx_tdd.h"
+#include "../rx_ucs2_misc.h"
+
+namespace rx{
+
+    inline void test_ucs2_misc_base_1(rx_tdd_t &rt)
+    {
+        uint32_t idx=query_ucs_block(1);
+        const ucs2_block_item_t &it=ucs2_blocks_range[idx];
+        rt.tdd_assert(idx!=ucs2_blocks_range_size);
+        rt.tdd_assert(it.code_begin==0);
+
+        //²âÊÔ×Ö·ûËùÊôunicode¿é
+        for(uint32_t c=0;c<0xFFFF;++c)
+        {
+            uint32_t idx=query_ucs_block(c);
+            const ucs2_block_item_t &it=ucs2_blocks_range[idx];
+            rt.tdd_assert(idx!=ucs2_blocks_range_size);
+            rt.tdd_assert(it.code_begin<=c);
+            const ucs2_block_item_t &itn=ucs2_blocks_range[idx+1];
+            rt.tdd_assert(itn.code_begin>c);
+        }
+
+        //²âÊÔ¿é±ßÔµ²éÕÒ
+        for(uint32_t i=0;i<ucs2_blocks_range_size;++i)
+        {
+            uint32_t c=ucs2_blocks_range[i].code_begin;
+            uint32_t idx=query_ucs_block_edge(c);
+            const ucs2_block_item_t &it=ucs2_blocks_range[idx];
+            rt.tdd_assert(idx!=ucs2_blocks_range_size);
+            rt.tdd_assert(it.code_begin==c);
+            const ucs2_block_item_t &itn=ucs2_blocks_range[idx+1];
+            rt.tdd_assert(itn.code_begin>c);
+        }
+    }
+
+}
+rx_tdd(ucs2_misc_base)
+{
+    rx::test_ucs2_misc_base_1(*this);
+}
+
+
+#endif
