684f38947dca582195581067b7773a3bb9d51799
Rain <hxrain@users.noreply.github.com>
2020-02-29 17:03:09 +0800

    准备编写标准功能的tcp服务器框架.

---
+++ rx_net_tcp_server.h
@@ -0,0 +1,16 @@
+#ifndef RX_NET_TCP_SERVER_H
+#define RX_NET_TCP_SERVER_H
+
+#include "rx_net_tcp_client.h"
+#include "rx_net_tcp_svrsock.h"
+
+namespace rx
+{
+    //-----------------------------------------------------
+    //多线程常规功能的tcp服务器,可完成通用的多线程服务器.
+    class tcp_server_t
+    {
+    };
+}
+
+#endif
